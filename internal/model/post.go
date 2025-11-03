98d31873e9d691e77e6290156e0978b37f2dadd0
juanplagos <juanlagospg@gmail.com>
2025-11-02 23:19:00 -0300

    feat: add blog post model

--- internal/model/post.go
+++ internal/model/post.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Post struct {
-    ID        int       `json:"id"`
-    Title     string    `json:"title"`
-    Slug      string    `json:"slug"`
-    Content   string    `json:"content"`
-    Author    string    `json:"author"`
+    ID int `json:"id"`
+    Title string `json:"title"`
+    Slug string `json:"slug"`
+    Body string `json:"body"`
+    Author string `json:"author"`
     CreatedAt time.Time `json:"created_at"`
 }
