37884bd2eb161fb67b258ee846b9eedc827d38b4
juanplagos <juanlagospg@gmail.com>
2025-11-04 08:19:00 -0300

    refactor: rename users and posts to authors and entries for compliance

---
+++ model/authors.go
@@ -0,0 +1,7 @@
+package model
+
+type Author struct {
+	Username string `json:"username"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
---
+++ model/entry.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Entry struct {
+    ID int `json:"id"`
+    Title string `json:"title"`
+    Slug string `json:"slug"`
+    Body string `json:"body"`
+    Author string `json:"author"`
+    CreatedAt time.Time `json:"created_at"`
+}
