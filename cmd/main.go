be81626bf5efa229116831a0700888496856c460
juanplagos <juanlagospg@gmail.com>
2025-11-04 09:50:18 -0300

    feat: add routes mux

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/juanplagos/bubble/router"
 )
 
 func main() {
-	
+	log.Println("server starting on ':8080'...")
+	mux := router.RegisterRoutes()
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+	    log.Fatal(err)
+	}
 }
