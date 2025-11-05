d2b607bd6373fe02b0ef40f877953baa3aafa391
juanplagos <juanlagospg@gmail.com>
2025-11-04 21:26:10 -0300

    feat: add db connection and entries repo interface

--- mock/mock_users.go
+++
@@ -1,7 +0,0 @@
-package mock
-
-import "github.com/juanplagos/bubble/model"
-
-var Author []model.Author = []model.Author{
-    {Username: "Bumblebee", Email: "bumblebee@autobots.com", Password: "1235"},
-}
