31874fdec82f1f7bc81aa0367d30fadbb6042683
juanplagos <juanlagospg@gmail.com>
2025-11-05 22:29:00 -0300

    refactor: move db connection pool initialization into its own file for better organization

---
+++ repository/postgres.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+    "github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitPostgresPool() *pgxpool.Pool {
+    user := os.Getenv("POSTGRES_USER")
+    password := os.Getenv("POSTGRES_PASSWORD")
+    host := os.Getenv("POSTGRES_HOST")
+    port := os.Getenv("POSTGRES_PORT")
+    dbname := os.Getenv("POSTGRES_DB")
+    
+    connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", 
+        user, password, host, port, dbname)
+    
+    pool, err := pgxpool.New(context.Background(), connStr)
+    if err != nil {
+        log.Fatalf("não foi possível criar a pool: %v\n", err)
+    }
+    
+    err = pool.Ping(context.Background())
+    if err != nil {
+        log.Fatalf("não foi possível pingar o banco de dados: %v\n", err)
+    }
+    
+    fmt.Println("pool do postgres pronta")
+    
+    return pool 
+}
