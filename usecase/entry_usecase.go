71fa788441d4b0c2679c4dd60f936fb3fde24980
juanplagos <juanlagospg@gmail.com>
2025-11-11 15:34:43 -0300

    refactor(usecase): update entry use case structure and add operations

--- usecase/entry_usecase.go
+++ usecase/entry_usecase.go
@@ -1,20 +1,40 @@
 package usecase
 
 import (
-	"github.com/juanplagos/bubble/repository"
 	"github.com/juanplagos/bubble/model"
+	"github.com/juanplagos/bubble/repository"
 )
 
-type EntryUseCase struct {
-	repo repository.PostgresEntryRepo
+type entryUseCase struct {
+	repo repository.EntryRepo
 }
 
-func NewEntryUseCase(repo repository.PostgresEntryRepo) EntryUseCase {
-	return EntryUseCase{
+func NewEntryUseCase(repo repository.EntryRepo) EntryUseCase {
+	return &entryUseCase{
 		repo: repo,
 	}
 }
 
-func (eu *EntryUseCase) GetAllEntries() ([]model.Entry, error) {
+func (eu *entryUseCase) GetAllEntries() ([]model.Entry, error) {
 	return eu.repo.GetAllEntries()
+}
+
+func (eu *entryUseCase) GetEntryById(id int) (model.Entry, error) {
+	return eu.repo.GetEntryById(id)
+}
+
+func (eu *entryUseCase) GetEntryBySlug(slug string) (model.Entry, error) {
+	return eu.repo.GetEntryBySlug(slug)
+}
+
+func (eu *entryUseCase) CreateEntry(entry *model.Entry) error {
+	return eu.repo.CreateEntry(entry)
+}
+
+func (eu *entryUseCase) UpdateEntry(id int, entry *model.Entry) error {
+	return eu.repo.UpdateEntry(id, entry)
+}
+
+func (eu *entryUseCase) DeleteEntry(id int) error {
+	return eu.repo.DeleteEntry(id)
 }
