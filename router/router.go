3c291e118aad00aed9c4ab38d2ccb236c2cb2abc
juanplagos <juanlagospg@gmail.com>
2025-11-11 15:34:17 -0300

    feat(router): register routes for entry and author handlers

--- router/router.go
+++ router/router.go
@@ -10,15 +10,29 @@ import (
 )
 
 func RegisterRoutes(pool *pgxpool.Pool) *http.ServeMux {
-	repo := repository.NewPostgresEntryRepo(pool)
+	entryRepo := repository.NewPostgresEntryRepo(pool)
+	authorRepo := repository.NewPostgresAuthorRepo(pool)
 
-	entryUseCase := usecase.NewEntryUseCase(*repo)
-	
+	entryUseCase := usecase.NewEntryUseCase(entryRepo)
+	authorUseCase := usecase.NewAuthorUseCase(authorRepo)
+
 	entryHandler := handler.NewEntryHandler(entryUseCase)
+	authorHandler := handler.NewAuthorHandler(authorUseCase)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /entries/", entryHandler.GetAll)
-	//mux.HandleFunc("GET /authors/", entryHandler.GetAuthors)
+	mux.HandleFunc("GET /entries/slug/", entryHandler.GetBySlug)
+	mux.HandleFunc("GET /entries", entryHandler.GetAll)
+	mux.HandleFunc("GET /entries/", entryHandler.GetByID)
+	mux.HandleFunc("POST /entries", entryHandler.Create)
+	mux.HandleFunc("PUT /entries/", entryHandler.Update)
+	mux.HandleFunc("DELETE /entries/", entryHandler.Delete)
+
+	mux.HandleFunc("GET /authors/email/", authorHandler.GetByEmail)
+	mux.HandleFunc("GET /authors", authorHandler.GetAll)
+	mux.HandleFunc("GET /authors/", authorHandler.GetByUsername)
+	mux.HandleFunc("POST /authors", authorHandler.Create)
+	mux.HandleFunc("PUT /authors/", authorHandler.Update)
+	mux.HandleFunc("DELETE /authors/", authorHandler.Delete)
 
 	return mux
 }
